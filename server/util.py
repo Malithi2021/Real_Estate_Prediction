32bf45c8ded4417b011634d04abf9fcd9a99d2d1
Malithi2021 <abayadeeramr.20@itfac.mrt.ac.lk>
2024-09-16 22:47:44 +0530

    postman test done

--- server/util.py
+++ server/util.py
@@ -19,7 +19,7 @@ def get_estimated_price(location,sqft,bhk,bath):
     if loc_index >=0:
         x[loc_index] = 1
 
-    return __model.predict([x])
+    return round(__model.predict([x])[0],2)
 
 def get_location_names():
     return __locations
@@ -33,6 +33,7 @@ def load_saved_artifacts():
        __data_columns= json.load(f)['data_columns']
        __locations = __data_columns[3:]
 
+    global __model
     with open("./artifacts/banglore_home_prices_model.pickle", 'rb') as f:
         __model = pickle.load(f)
     print("loading saved artifacts....end")
@@ -40,3 +41,7 @@ def load_saved_artifacts():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar','1000',3,3))
+    print(get_estimated_price('1st Phase JP Nagar', '1000', 2, 2))
+    print(get_estimated_price('Kalhalli', '1000', 2, 2))
+    print(get_estimated_price('Ejipura', '1000', 2, 2))
